9fbf8b67e93bd6b2d85217e96604e36849db9b99
Dave Njoroge <davenjoroge10@gmail.com>
2024-11-06 11:59:36 +0300

    minor fix

--- front/composables/useMoment.ts
+++ front/composables/useMoment.ts
@@ -1,13 +1,11 @@
 import moment from "moment";
 
 const useMoment = () => {
-    moment.locale("en", {
+    moment.updateLocale("en", {
       week: {
         dow: 6,
       },
     });
-
-    moment.locale("en");
 
     return moment
 }
