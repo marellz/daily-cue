4fba8f900badcc4bfceb6bdffbdb0ded9fecaaa8
Dave Njoroge <davenjoroge10@gmail.com>
2025-02-06 23:43:32 +0300

    task store massive update: filters, update, destroy and others

--- store/tasks.ts
+++ store/tasks.ts
@@ -1,3 +1,4 @@
+import type { TaskStatus } from "~/types/task";
 import { useAuthStore } from "./auth";
 import { useTagsStore } from "./tags";
 import { useToastsStore } from "./toasts";
@@ -10,16 +11,24 @@ export interface TaskForm {
   user_id: string;
   id?: string;
   due_date: string;
+  completed?: boolean;
+}
+
+export interface TaskFilter {
+  tags: string[];
+  status: TaskStatus | "default";
+  date: string;
 }
 
 export interface Task {
-  id?: string;
-  completed?: boolean | null;
-  created_at?: string;
+  id: string;
+  completed: boolean;
+  created_at: string;
   description?: string | null;
   status: string;
-  tags?: string[] | null;
+  tags: string[];
   title: string;
+  due_date: string;
   user_id: string;
 }
 
@@ -27,17 +36,40 @@ export const useTasksStore = defineStore(
   "tasks",
   () => {
     const auth = useAuthStore();
-    const tags = useTagsStore();
     const toasts = useToastsStore();
     const supabase = useSupabaseClient();
+    const moment = useMoment();
     const loading = ref(false);
 
-    const weeklyActivity = ref([]);
     const tasks = ref<Task[]>([]);
 
+    const today = computed(() => {
+      const today = moment();
+      const startDate = today.clone().startOf("day").toISOString();
+      const endDate = today.clone().endOf("day").toISOString();
+
+      return {
+        from: startDate,
+        to: endDate,
+      };
+    });
+
+
+    const filters = ref<TaskFilter>({
+      status: "default",
+      date: moment().toISOString(),
+      tags: [],
+    });
+
+
+    const updateFilters = (newFilters: TaskFilter) => {
+      filters.value = newFilters
+    }
+
     const get = async (
-      range: { from: string; to: string } | null = null,
-      status: string = 'default'
+      range: { from: string; to: string } | null = today.value,
+      status: string = "default",
+      tags: string[] | null = []
     ) => {
       if (!auth.user) {
         handleError("Not authenticated");
@@ -52,21 +84,25 @@ export const useTasksStore = defineStore(
 
         // filters
 
-        if(range){
-          const startOfFrom = range.from
-          const endOfTo = range.to
+        if (range) {
+          const startOf = range.from;
+          const endOf = range.to;
+
+
+          console.log(range)
+
           // start of from
-          query.rangeGte('due_date', startOfFrom)
+          query.gte("due_date", startOf);
 
           // end of to
-          query.rangeLte('due_date', endOfTo)
+          query.lte("due_date", endOf);
         }
 
-        if(status !== 'default'){
-          query.eq('status', status)
+        if (status !== "default") {
+          query.eq("status", status);
         }
 
-        const { data, error } = await query
+        const { data, error } = await query;
 
         if (error) {
           handleError(error.message);
@@ -101,9 +137,11 @@ export const useTasksStore = defineStore(
         if (error) {
           handleError(error.message);
         }
-        if (data) {
-          tasks.value = data;
+        if (data?.length) {
+          return data[0];
         }
+
+        return null;
       } catch (error) {
         handleError(error);
       } finally {
@@ -132,6 +170,7 @@ export const useTasksStore = defineStore(
         }
         if (data) {
           tasks.value = [...tasks.value, ...data];
+          toasts.addSuccess("Created", "Task was successfully created!");
         }
 
         return true;
@@ -142,25 +181,97 @@ export const useTasksStore = defineStore(
       }
     };
 
-    const update = async () => {};
+    const update = async (id: string, form: TaskForm) => {
+      if (!auth.user) {
+        handleError("Not authenticated");
+        return;
+      }
 
-    const setDateRange = async () => {};
+      loading.value = true;
+
+      try {
+        const { status, error } = await supabase
+          .from("tasks")
+          .update(form)
+          .eq("id", id);
+
+        if (error) {
+          handleError(error.message);
+          return false;
+        }
+        if (status === 204) {
+          tasks.value = tasks.value.map((t) => {
+            return t.id === id ? { ...t, ...form } : t;
+          });
+
+          toasts.addSuccess("Updated", "Task was successfully updated!");
+
+          return true;
+        }
+
+        return true;
+      } catch (error) {
+        handleError(error);
+      } finally {
+        loading.value = false;
+      }
+    };
+
+    const destroy = async (id: string) => {
+      if (!auth.user) {
+        handleError("Not authenticated");
+        return;
+      }
+
+      loading.value = true;
+
+      try {
+        const { status, error } = await supabase
+          .from("tasks")
+          .delete()
+          .eq("id", id);
+
+        if (error) {
+          handleError(error.message);
+          return false;
+        }
+        if (status === 204) {
+          let _updatedIndex = tasks.value.findIndex((t) => t.id === id);
+          tasks.value = [...tasks.value.splice(_updatedIndex, 1)];
+
+          toasts.addSuccess("Deleted", "Task was successfully deleted!");
+
+          return true;
+        }
+
+        return true;
+      } catch (error) {
+        handleError(error);
+      } finally {
+        loading.value = false;
+      }
+    };
 
     const handleError = (message: string | unknown = "Unknown error") => {
-      console.error({ error: "Tags store error", message });
+      console.error({ error: "Tasks store error", message });
       toasts.addError(
-        "Tags error",
+        "Tasks error",
         typeof message === "string" ? message : "Unknown"
       );
     };
 
     return {
+      today,
       tasks,
       get,
       getBy,
       create,
       update,
-      setDateRange,
+      destroy,
+
+      // 
+      filters,
+      updateFilters
     };
   },
   {
